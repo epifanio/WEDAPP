6560f9a0e6bc33e7c1d516dcc6fb5fbd805c8c44
epifanio <epiesasha@me.com>
2024-04-12 18:51:15 +0000

    adding some raw css

---
+++ docker/ui/app/hv_css.py
@@ -0,0 +1,49 @@
+raw_css = """
+body {
+  color: blue;
+}
+
+h1 {
+  color: green;
+}
+
+h2 {
+  color: green;
+}
+
+.bk-header {
+    font-family: Ubuntu;
+    font-weight: bold;
+    font-size: 20px;
+    'background-color': '#F6F6F6', 
+    'border': '1px solid grey',
+    'border-radius': '5px', 
+    'padding': '10px',
+}
+
+.bk-panel-models-markup-HTML {
+    font-size: 15px;
+}
+
+.bk-Checkbox {
+    font-size: 15px;
+}
+
+.bk-btn bk-btn-default {
+    font-size: 20px;
+}
+
+.bk-btn-group {
+    font-size: 20px;
+}
+@viewport {
+  width: device-width;
+  min-zoom: 1;
+  zoom: 1;
+}
+.viewport {
+  width: device-width;
+  min-zoom: 1;
+  zoom: 1;
+}
+"""
