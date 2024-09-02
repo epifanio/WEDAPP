30208f261c6a4742577eb3c378c3be5980ee6130
epifanio <epiesasha@me.com>
2024-09-02 17:14:41 +0000

    fix typos and add reference for pay to

--- docker/ui/app/translator.py
+++ docker/ui/app/translator.py
@@ -14,11 +14,11 @@ def intro_text(lang='en'):
         <br>
         <b>12:00 Paseo del Prado 48.</b> Fear not! From this point on we’ll be on wheels.<br>
         <br>
-        <b>14:00 Seeds Restauran.</b> 
+        <b>14:00 Seeds Restaurant.</b> 
         The reception will include a grand banquet and a surprise-packed afternoon.<br>
         <br>
         <b>22:00 Black Jack Club</b> (better known to us hard-core Madridians as the Cat Temple). 
-        Those of you who can handle it are invited to join us there at the after party!<br>
+        Those of you who can handle it are invited to join us there for the after party!<br>
         <br>
         See you on <b>June 15, 2024!</b><br>
         <br>
@@ -124,7 +124,7 @@ def optional_separator_text(lang='en'):
         text = """<p><h2><b>Logistic support and bits not included</b></h2></p>
         <hr style="height: 5px; background-color: #3498db;">"""
     if lang == 'es':
-        text = """<p><h2><b>Apoyo logístico a y cosas que no están incluidas</b></h2></p>
+        text = """<p><h2><b>Apoyo logístico y cosas que no están incluidas</b></h2></p>
         <hr style="height: 5px; background-color: #3498db;">"""
     if lang == 'it':
         text = """<p><h2><b>Appoggio logistico e cose che non sono incluse</b></h2></p>
@@ -134,57 +134,60 @@ def optional_separator_text(lang='en'):
 def html_donation_text(lang='en'):
     text = ''
     if lang == 'en':
-        text = """ <center><h2>Donation are welcome!</h2></center> <br> 
+        text = """ <center><h2>Donation welcome!</h2></center> <br> 
         <p style="font-family:'Courier New'; font-size:20px;"> 
-        Bank details:<br>
         <br>
         For <i>Bizum</1> use this number: <br>
         (+34) 686269566 <br>
         <br>
         For <i>Euro</i> transfer use this account number: <br> 
         ES37 0073 0100 5704 6207 6121 <br>
+        Payable to: Genoveva Gonzalez Mirelis<br>
         <br>
         For <i>Vipps</i> use this number: <br> 
         (+47) 47669874 <br>
         <br>
         For <i>Kroner</i> transfer use this account number: <br> 
         NO93 6031 1712 228 <br>
+        Payable to: Genoveva Gonzalez Mirelis<br>
         <br>
         </p>"""
     if lang == 'es':
         text = """ <center><h2>Se admiten donativos!</h2></center> <br> 
         <p style="font-family:'Courier New'; font-size:20px;"> 
-        Bank details:<br>
         <br>
         Para hacer un <i>Bizum</1> usa este número: <br>
         (+34) 686269566 <br>
         <br>
         Para hacer una transferencia en <i>Euro</i> usa este número de cuenta: <br> 
         ES37 0073 0100 5704 6207 6121 <br>
+        A nombre de: Genoveva González Mirelis<br>
         <br>
         Para hacer un <i>Vipps</i> usa este número: <br> 
         (+47) 47669874 <br>
         <br>
         Para hacer una transferencia en <i>Corona noruegas</i> usa este número de cuenta: <br> 
         NO93 6031 1712 228 <br>
+        A nombre de: Genoveva González Mirelis<br>
         <br>
         </p>"""
     if lang == 'it':
         text = """ <center><h2>Donazioni sono le benvenute!</h2></center> <br> 
         <p style="font-family:'Courier New'; font-size:20px;"> 
-        Bank details:<br>
         <br>
         Per <i>Bizum</1> usa questo numero: <br>
         (+34) 686269566 <br>
         <br>
         Per trasferimenti in <i>Euro</i> usa questo numero di conto: <br> 
         ES37 0073 0100 5704 6207 6121  <br>
+        Intestato a: Genoveva Gonzalez Mirelis<br>
         <br>
         Per <i>Vipps</i> usa questo numero: <br> 
         (+47) 47669874 <br>
         <br>
         Per trasferimenti in <i>Corone Norvegesi</i> usa questo numero di conto: <br> 
         NO93 6031 1712 228 <br>
+        Intestato a: Genoveva Gonzalez Mirelis<br>
         <br>
         </p>"""
     return text
@@ -195,22 +198,22 @@ def change_widget_labels_text(lang='en'):
         widget_labels['name_label'] = 'Name'
         widget_labels['surname_label'] = "Last Name"
         widget_labels['age_options_label'] = "Age Group"
-        widget_labels['cerimony'] = "Yes, I will attend the ceremony at the District Board *"
+        widget_labels['cerimony'] = "Yes, I will attend the ceremony at the District Board"
         widget_labels['banquet'] = "Yes, of course I will attend the banquet! What a question!"
         widget_labels['food_restrictions'] = "Yes, I have dietary restrictions"
         widget_labels['food_restrictions_details_label'] = "Dietary Restrictions"
         widget_labels['allergy'] = "Yes, I have some food allergies or intolerances"
         widget_labels['allergy_details_label'] = "Allergies or Intolerance"
-        widget_labels['party'] = "Yes, I will go to the after party. Let's give it our all! **"
+        widget_labels['party'] = "Yes, I will go to the after party. Let's give it our all! *"
         widget_labels['lunch'] = "Yes, I'm up for lunch on Friday the 14th" # (on my own, at a restaurant yet to be decided)
         widget_labels['dinner'] = "Yes, I'm up for dinner on Friday the 14th" # (on my own, at a restaurant yet to be decided)
         widget_labels['hotel'] = "Yes, I would like you to help me book a hotel near Massi and Geno's house"
         widget_labels['room_label'] = "Room Type"
         widget_labels['date_range_picker_label'] = "Arrival and Departure Date"
         widget_labels['number_guest_label'] = "Number of Guests"
-        widget_labels['foot_note_1_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>*: Attention! Capacity is limited! <br> We greatly appreciate if you prioritize those coming from outside Madrid :-)</b>"""
-        widget_labels['foot_note_2_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>**: Attention! By this time, the little ones should already be in bed, <br> so only adults can attend this part of the event!</b>"""
-        widget_labels['foot_note_3_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>***: On my own, at a restaurant yet to be decided!</b>"""
+        # widget_labels['foot_note_1_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>*: Attention! Capacity is limited! <br> We greatly appreciate if you prioritize those coming from outside Madrid :-)</b>"""
+        widget_labels['foot_note_2_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>*: Attention! By this time, the little ones should already be in bed, <br> so only adults can attend this part of the event!</b>"""
+        widget_labels['foot_note_3_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>**: On my own, at a restaurant yet to be decided!</b>"""
         retiro_description_pane, restaurant_description_pane, casa_sposi_description_pane, black_jack_description_pane, pit_stop_description_pane = location_description(lang='en')
         widget_labels['retiro_description_pane'] = retiro_description_pane
         widget_labels['restaurant_description_pane'] = restaurant_description_pane
@@ -233,22 +236,22 @@ def change_widget_labels_text(lang='en'):
         widget_labels['name_label'] = 'Nome'
         widget_labels['surname_label'] = "Cognome"
         widget_labels['age_options_label'] = "Gruppo di età"
-        widget_labels['cerimony'] = "Sì, parteciperò alla cerimonia presso la Giunta Distrettuale *"
+        widget_labels['cerimony'] = "Sì, parteciperò alla cerimonia presso la Giunta Distrettuale"
         widget_labels['banquet'] = "Sì, naturalmente parteciperò al banchetto! Che domanda!"
         widget_labels['food_restrictions'] = "Sì, ho restrizioni alimentari"
         widget_labels['food_restrictions_details_label'] = "Restrizioni Alimentari"
         widget_labels['allergy'] = "Sì, ho qualche allergia o intolleranza alimentare"
         widget_labels['allergy_details_label'] = "Allergie o Intolleranze"
-        widget_labels['party'] = "Sì, parteciperò alla festa post-matrimonio. Diamo il massimo! **"
+        widget_labels['party'] = "Sì, parteciperò alla festa post-matrimonio. Diamo il massimo! *"
         widget_labels['lunch'] = "Sì, sono disponibile per pranzo venerdì 14" # (da solo, in un ristorante ancora da decidere)
         widget_labels['dinner'] = "Sì, sono disponibile per cena venerdì 14" # (da solo, in un ristorante ancora da decidere)
         widget_labels['hotel'] = "Sì, mi piacerebbe che mi aiutassi a prenotare un hotel vicino alla casa di Massi e Geno"
         widget_labels['room_label'] = "Tipo di Camera"
         widget_labels['date_range_picker_label'] = "Data di Arrivo e Partenza"
         widget_labels['number_guest_label'] = "Numero di Ospiti"
-        widget_labels['foot_note_1_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>*: Attenzione! La capacità è limitata! <br> Apprezziamo molto se dai priorità a chi viene da fuori Madrid :-)</b>"""
-        widget_labels['foot_note_2_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>**: Attenzione! A quest'ora i più piccoli dovrebbero già essere a letto, <br> quindi solo gli adulti possono partecipare a questa parte dell'evento!</b>"""
-        widget_labels['foot_note_3_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>***: Da solo, in un ristorante ancora da decidere!</b>"""
+        #widget_labels['foot_note_1_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>*: Attenzione! La capacità è limitata! <br> Apprezziamo molto se dai priorità a chi viene da fuori Madrid :-)</b>"""
+        widget_labels['foot_note_2_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>*: Attenzione! A quest'ora i più piccoli dovrebbero già essere a letto, <br> quindi solo gli adulti possono partecipare a questa parte dell'evento!</b>"""
+        widget_labels['foot_note_3_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>**: Da solo, in un ristorante ancora da decidere!</b>"""
         retiro_description_pane, restaurant_description_pane, casa_sposi_description_pane, black_jack_description_pane, pit_stop_description_pane = location_description(lang='it')
         widget_labels['retiro_description_pane'] = retiro_description_pane
         widget_labels['restaurant_description_pane'] = restaurant_description_pane
@@ -271,22 +274,22 @@ def change_widget_labels_text(lang='en'):
         widget_labels['name_label'] = 'Nombre'
         widget_labels['surname_label'] = "Apellidos"
         widget_labels['age_options_label'] = "Clase de edad"
-        widget_labels['cerimony'] = "Sí, asistiré a la ceremonia en la Junta de Distrito *"
+        widget_labels['cerimony'] = "Sí, asistiré a la ceremonia en la Junta de Distrito"
         widget_labels['banquet'] = "Sí, por supuesto que asistiré al banquete! Qué pregunta!"
         widget_labels['food_restrictions'] = "Sí, tengo restricciones alimentarias"
         widget_labels['food_restrictions_details_label'] = "Restricciones alimentarias"
         widget_labels['allergy'] = "Sí, tengo alguna alergia o intolerancia alimentaria"
         widget_labels['allergy_details_label'] = "Alergias o intolerancia"
-        widget_labels['party'] = "Sí, iré a la fiesta post-boda. A darlo todo! **"
+        widget_labels['party'] = "Sí, iré a la fiesta post-boda. A darlo todo! *"
         widget_labels['lunch'] = "Sí, me apunto a una comida el viernes 14" # (por cuenta propia, en un restaurante aún por decidir)
         widget_labels['dinner'] = "Sí, me apunto a una cena el viernes 14" # (por cuenta propia, en un restaurante aún por decidir)
         widget_labels['hotel'] = "Sí, me gustaría que me ayudaras a reservar hotel cerca de la casa de Massi y Geno"
         widget_labels['room_label'] = "Tipo de habitación"
         widget_labels['date_range_picker_label'] = "Dia de llegada y partida"
         widget_labels['number_guest_label'] = "Número de huéspedes"
-        widget_labels['foot_note_1_description'] = """<p style="font-family:'Courier New'; font-size:15px;"> <b><font color='orange'>*: Atención! El aforo es limitado! <br> Os agradecemos infinito si le dáis prioridad a los que vienen de fuera de Madrid :-)</b></p>"""
-        widget_labels['foot_note_2_description'] = """<p style="font-family:'Courier New'; font-size:15px;"> <b><font color='orange'>**: Atención! A estas horas los peques ya tienen que estar en la cama, <br> así que a esta parte del evento solo podrán asistir los mayores!</b>"""
-        widget_labels['foot_note_3_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>***: Por cuenta propia, en un restaurante aún por decidir!</b>"""
+        #widget_labels['foot_note_1_description'] = """<p style="font-family:'Courier New'; font-size:15px;"> <b><font color='orange'>*: Atención! El aforo es limitado! <br> Os agradecemos infinito si le dáis prioridad a los que vienen de fuera de Madrid :-)</b></p>"""
+        widget_labels['foot_note_2_description'] = """<p style="font-family:'Courier New'; font-size:15px;"> <b><font color='orange'>*: Atención! A estas horas los peques ya tienen que estar en la cama, <br> así que a esta parte del evento solo podrán asistir los mayores!</b>"""
+        widget_labels['foot_note_3_description'] = """<p style="font-family:'Courier New'; font-size:15px;"><b><font color='orange'>**: Por cuenta propia, en un restaurante aún por decidir!</b>"""
         retiro_description_pane, restaurant_description_pane, casa_sposi_description_pane, black_jack_description_pane, pit_stop_description_pane = location_description(lang='es')
         widget_labels['retiro_description_pane'] = retiro_description_pane
         widget_labels['restaurant_description_pane'] = restaurant_description_pane
